c675924db04d7054c2ea1f810a09435a4a22f597
Ahmed Mohamed <118309402+amedmohmed925@users.noreply.github.com>
2024-07-21 04:03:33 +0200

    commit2

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const sr = ScrollReveal({
+    origin: 'bottom', 
+    distance: '50px',
+    duration: 1500,
+    delay: 300, 
+    reset: false 
+  });
+  
+  sr.reveal('section', {
+    interval: 400 
+  });
+  
